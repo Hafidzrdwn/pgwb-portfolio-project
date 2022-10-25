a465d1dafbde845c061c19e90aa48ce1ea43b250
Hafidzrdwn <hafidz10032005@gmail.com>
2022-10-25 14:08:31 +0700

    change users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('image')->default('default.jpg');
+            $table->string('role')->default('admin');
             $table->rememberToken();
             $table->timestamps();
         });
